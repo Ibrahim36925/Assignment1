d257671f241a9bed14fac3948630d00b53e10604
Ibrahim Ahmed <mohammadibrahimahmed135@gmail.com>
2022-05-22 12:12:30 -0700

    file created

---
+++ java.web/script.js
@@ -0,0 +1,46 @@
+//Chapter1
+//Question1
+alert("Welcome to my Website");
+//Question2
+alert("Error! Plzz Enter a Valid Password");
+//Question3
+alert("Welcome to JS Land" + "\n" + "Happy Coding");
+//Question4
+alert("Welcome to JS Land")
+alert("Hppy Coding");
+//Question5
+alert("Hello..I can run js through console")
+console.log("Hello World");
+//Question6
+//  <h1>Assignment-1</h1>
+//  <p>Hello World</p>
+// d:body (after your page's HTML)
+//Chapter2
+//Question1
+var username;
+//Question2
+ var name = "Muhammad Ibrahim"
+ //Question3
+ var message = "Hello World"
+  //Question4
+  var name = "Muhammad Ibrahim"
+  alert(name)
+  var age = "17 years old"
+  alert(age)
+  var study = " Certified Web Development"
+  alert(study)
+   //Question5
+   var message = "PIZZA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" + "\n" + "P"
+   alert(message); 
+   //Question6 
+   var email = "mohammadibrahimahmed135@gmail.com"
+   alert("My E-mail address is " + email)
+      //Question7
+      var book = "A Smarter Way To Learn Javascript" 
+      alert("I am trying to learn from the Book" + "\n" + book)
+      //Question8
+      var message = "Yah!I can write HTML elements through JS"
+      document.write(message)
+      // Question 9
+var pattern = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬" ;
+alert(pattern);
